f0b660a791457d7e2f436dfea391abccd54f0aa8
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-16 19:38:33 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ * Return: is 0
+ */
+
+int main (void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+
+}
