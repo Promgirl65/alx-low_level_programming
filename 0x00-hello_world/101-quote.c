c49aae95a21485f4981268d9bbd23313ee5e68e2
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-18 15:13:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  * main - Entry point
  * Return: 1
  */
-int main(int nb)
+int main(int 1)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (nb > 0);
+return (1);
 }
