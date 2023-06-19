e8a14ad1c2a1aaa845794736950bdda7ca9ff761
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-19 16:16:47 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - this is the entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d\n", n);
+	return (0);
+}
