185e73e7641779c229efed1ce5b57954c0a9521a
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-20 16:53:19 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,10 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @j: the character to be checked
- * Return: 1 if j is a letter, 0 otherwise
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-int _isalpha(int j);
+int _isalpha(int c)
 {
-	return ((j >= 'a' && j <= 'z') || (j >= 'A' && j <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
