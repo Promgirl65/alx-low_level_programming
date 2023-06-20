707f2c7cba4745aaf0db5c66640fb346af6ab66d
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-20 14:48:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ * Description - prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char lowcase;
+
+	for(lowcase = 'a'; lowcase <= 'z'; lowcase++)
+		_putchar(lowercase);
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+The 0x02 C - Functions,nested loops project describes;
+Nested while loops, C - Functions, The purpose of a function prototype and C -Header Files
