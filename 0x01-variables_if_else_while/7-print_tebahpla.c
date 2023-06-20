dbd35ae2f82f7a3245d9c68b262713bc6f31b598
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-19 17:23:47 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - this is the entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lcase;
+
+	for (lcase = 'z lcase >= 'a'; lcase--)
+		putchar(lcase);
+	putchar('\n');
+
+	return (0);
+}
+
+
