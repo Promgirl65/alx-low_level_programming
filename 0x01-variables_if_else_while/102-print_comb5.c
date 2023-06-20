2d4c5b4dd8c8ffb7ff0ac002425e66e02be76ca3
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-19 19:29:47 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -this is the entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y, z;
+	for (y = 0;  z <= 98; y++)
+	{
+
+		for (z + y + 1; z <= 99; z++)
+		{
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			putchar(' ');
+			putchar((z / 10) + '0');
+			putchar((z % 10) + '0');
+			if (y == 98 && z ==99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
