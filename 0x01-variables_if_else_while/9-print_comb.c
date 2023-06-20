49b81bc817a2d04a686370c72cb48696c9c31f9c
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-19 18:22:09 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - this is the entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		putchar(d);
+		id (j != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
