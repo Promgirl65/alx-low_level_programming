9aa5c8513ab097d81ce9358ed23cf57db10ccca5
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-21 02:09:34 -0700

    7-main.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * Return: value of the last digit
+ */
+
+int print_last_digit(int d);
+{
+	int i;
+
+	if (d < 0)
+		d = -d;
+
+	i = d % 10;
+
+	if (i < 0)
+		i = -i;
+	
+	_putchar(i + '0');
+
+	return (i);
+}
