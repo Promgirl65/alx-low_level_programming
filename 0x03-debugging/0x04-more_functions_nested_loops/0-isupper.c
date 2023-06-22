7f8e24e937b9efaf1357671e7d98317d8b4ea5cb
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-22 15:57:26 -0700

    0-isupper.c

--- 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,14 @@
 #include "main.h"
 /**
 * _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
+* @c: char to check
+* Return: Always 0 or 1
 */
 int _isupper(int c)
 {
-
-
 	if (c >= 'A' && c <= 'Z')
-	{
-	return (1);
-	}
-	else
-{
-	return (0);
-}
+		return (1);
 
+	else
+		return (0);
 }
