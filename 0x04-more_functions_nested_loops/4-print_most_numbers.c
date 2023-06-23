93d2fd92ba264eedb0fb725ebaf0b1389af7b7a7
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-23 16:46:21 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_most_numbers prints num except 2 and 4
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '48'; c <= '58'; c++)
+	for (c = 48; c < 58; c++)
 	{
 		if (c != 50)
 		{
