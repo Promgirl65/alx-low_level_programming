a6902ee76164d6321e077ece2f8f2afeadbc02fc
Promgirl65 <olayinkabusolami@gmail.com>
2023-06-23 16:30:35 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: a -  first variable
+ * @b: b - second variable
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
