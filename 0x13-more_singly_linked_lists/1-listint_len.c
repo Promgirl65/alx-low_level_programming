66e25de9b0800ef6dba49c89757fed6ff27abecd
Promgirl65 <olayinkabusolami@gmail.com>
2023-08-01 23:17:03 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * listint_len - returns the number of elements
+ * of a linked listint_t list
  * @h: linked list of listint_t
  *
  * Return: the number of nodes
  *
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nn = 0;
-	
+
 	while (h != NULL)
 	{
 		nn++;
 		h = h->next;
 	}
-	
+
 	return (nn);
 }
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: points to the first element in the list
+ * @n: data to insert in the new element
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_Node;
+	listint_t *temp = *head ;
+
+	new_Node = malloc(sizeof(listint_t));
+	if (!new_Node)
+		return (NULL);
+
+	new_Node->n = n;
+	new_Node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_Node;
+		return (new_Node);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_Node;
+
+
+	return (new_Node);
+}
